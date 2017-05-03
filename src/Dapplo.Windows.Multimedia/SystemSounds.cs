f19ebec092214549bdc1d44cbf09f264f78bf47a
Robin <robin@dapplo.net>
2017-05-03 10:14:48 +0200

    Fixed missing XML documentation. [release]

--- src/Dapplo.Windows.Multimedia/SystemSounds.cs
+++ src/Dapplo.Windows.Multimedia/SystemSounds.cs
@@ -26,13 +26,44 @@ namespace Dapplo.Windows.Multimedia
     /// </summary>
     public enum SystemSounds
     {
+        /// <summary>
+        /// Sound/event that is associated with the Windows "Information" alert type.
+        /// </summary>
         SystemAsterisk,
+
+        /// <summary>
+        /// Sound/event that is associated with the Windows "default" alert type.
+        /// </summary>
         SystemDefault,
+
+        /// <summary>
+        /// Sound/event that is associated with the Windows "Exclamation" alert type.
+        /// </summary>
         SystemExclamation,
+
+        /// <summary>
+        /// Sound/event that is associated with the Windows "Exit" alert type.
+        /// </summary>
         SystemExit,
+
+        /// <summary>
+        /// Sound/event that is associated with the Windows "Hand" alert type.
+        /// </summary>
         SystemHand,
+
+        /// <summary>
+        /// Sound/event that is associated with the Windows "Question" alert type.
+        /// </summary>
         SystemQuestion,
+
+        /// <summary>
+        /// Sound/event that is associated with the Windows "Start".
+        /// </summary>
         SystemStart,
+
+        /// <summary>
+        /// Sound/event that is associated with the Windows "Welcome".
+        /// </summary>
         SystemWelcome
     }
 }
