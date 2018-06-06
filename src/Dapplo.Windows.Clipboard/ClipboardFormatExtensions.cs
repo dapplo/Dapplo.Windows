3fb1c5f8e88780ed8100e2935680543ce2ff3292
Robin <robin@dapplo.net>
2018-06-06 22:07:02 +0200

    Improvement for the clipboard format lists

--- src/Dapplo.Windows.Clipboard/ClipboardFormatExtensions.cs
+++ src/Dapplo.Windows.Clipboard/ClipboardFormatExtensions.cs
@@ -73,7 +73,7 @@ namespace Dapplo.Windows.Clipboard
         /// Method to map a clipboard format to an ID
         /// </summary>
         /// <param name="format">clipboard format</param>
-        /// <returns>dtring with the id</returns>
+        /// <returns>uint with the id</returns>
         public static uint MapFormatToId(string format)
         {
             if (!Format2Id.TryGetValue(format, out var formatId))
@@ -82,6 +82,36 @@ namespace Dapplo.Windows.Clipboard
             }
 
             return formatId;
+        }
+
+        /// <summary>
+        /// Method to map a clipboard ID to a format name
+        /// </summary>
+        /// <param name="formatId">clipboard format ID</param>
+        /// <returns>string with the format</returns>
+        public static string MapIdToFormat(uint formatId)
+        {
+            if (Id2Format.TryGetValue(formatId, out var format))
+            {
+                return format;
+            }
+
+            unsafe
+            {
+                const int capacity = 256;
+                var clipboardFormatName = stackalloc char[capacity];
+                var nrCharacters = NativeMethods.GetClipboardFormatName(formatId, clipboardFormatName, capacity);
+                if (nrCharacters <= 0)
+                {
+                    return null;
+                }
+                // No name
+                format = new string(clipboardFormatName, 0, nrCharacters);
+                Id2Format[formatId] = format;
+                Format2Id[format] = formatId;
+            }
+
+            return format;
         }
 
         /// <summary>
@@ -94,14 +124,16 @@ namespace Dapplo.Windows.Clipboard
             uint clipboardFormatId;
             lock (Lock)
             {
-                if (!Format2Id.TryGetValue(format, out clipboardFormatId))
+                if (Format2Id.TryGetValue(format, out clipboardFormatId))
                 {
-                    clipboardFormatId = NativeMethods.RegisterClipboardFormat(format);
-
-                    // Make sure the format is known
-                    Id2Format[clipboardFormatId] = format;
-                    Format2Id[format] = clipboardFormatId;
+                    return clipboardFormatId;
                 }
+
+                clipboardFormatId = NativeMethods.RegisterClipboardFormat(format);
+
+                // Make sure the format is known
+                Id2Format[clipboardFormatId] = format;
+                Format2Id[format] = clipboardFormatId;
             }
 
             return clipboardFormatId;
@@ -111,51 +143,39 @@ namespace Dapplo.Windows.Clipboard
         ///     Enumerate through all formats on the clipboard, assumes the clipboard was already locked.
         /// </summary>
         /// <returns>IEnumerable with strings defining the format</returns>
-        public static IList<string> AvailableFormats(this IClipboardAccessToken clipboardAccessToken)
+        public static IEnumerable<string> AvailableFormats(this IClipboardAccessToken clipboardAccessToken)
+        {
+            clipboardAccessToken.ThrowWhenNoAccess();
+
+            return clipboardAccessToken.AvailableFormatIds().Select(MapIdToFormat).Where(format => !string.IsNullOrEmpty(format));
+        }
+
+        /// <summary>
+        ///     Enumerate through all formats on the clipboard, assumes the clipboard was already locked.
+        /// </summary>
+        /// <returns>IEnumerable with strings defining the format</returns>
+        public static IEnumerable<uint> AvailableFormatIds(this IClipboardAccessToken clipboardAccessToken)
         {
             clipboardAccessToken.ThrowWhenNoAccess();
 
             uint clipboardFormatId = 0;
 
-            var result = new List<string>();
-            unsafe
+            while (true)
             {
-                const int capacity = 256;
-                var clipboardFormatName = stackalloc char[capacity];
-                while (true)
+                clipboardFormatId = NativeMethods.EnumClipboardFormats(clipboardFormatId);
+                if (clipboardFormatId == 0)
                 {
-                    clipboardFormatId = NativeMethods.EnumClipboardFormats(clipboardFormatId);
-                    if (clipboardFormatId == 0)
+                    // If GetLastWin32Error return SuccessError, this is the end
+                    if (Marshal.GetLastWin32Error() == SuccessError)
                     {
-                        // If GetLastWin32Error return SuccessError, this is the end
-                        if (Marshal.GetLastWin32Error() == SuccessError)
-                        {
-                            break;
-                        }
-                        // GetLastWin32Error didn't return SuccessError, so throw exception
-                        throw new Win32Exception();
+                        yield break;
                     }
-
-                    if (Id2Format.TryGetValue(clipboardFormatId, out var formatName))
-                    {
-                        result.Add(formatName);
-                        continue;
-                    }
-
-                    var nrCharacters = NativeMethods.GetClipboardFormatName(clipboardFormatId, clipboardFormatName, capacity);
-                    if (nrCharacters <= 0)
-                    {
-                        // No name
-                        continue;
-                    }
-                    formatName = new string(clipboardFormatName, 0, nrCharacters);
-                    Id2Format[clipboardFormatId] = formatName;
-                    Format2Id[formatName] = clipboardFormatId;
-                    result.Add(formatName);
+                    // GetLastWin32Error didn't return SuccessError, so throw exception
+                    throw new Win32Exception();
                 }
-            }
 
-            return result;
+                yield return clipboardFormatId;
+            }
         }
     }
 }
