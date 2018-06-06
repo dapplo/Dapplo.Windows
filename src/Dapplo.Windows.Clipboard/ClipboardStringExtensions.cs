86b4423e5b8a2644346808a55207bb9ed64a6fd0
Robin <robin@dapplo.net>
2018-06-06 09:28:44 +0200

    Added missing overloads to specify the clipboard format when setting the content as string. [release]

--- src/Dapplo.Windows.Clipboard/ClipboardStringExtensions.cs
+++ src/Dapplo.Windows.Clipboard/ClipboardStringExtensions.cs
@@ -30,15 +30,48 @@ namespace Dapplo.Windows.Clipboard
     {
         /// <summary>
         /// Place string on the clipboard, this assumes you already locked the clipboard.
-        /// It uses CF_UNICODETEXT by default, as all other formats are automatically generated from this by Windows.
         /// </summary>
         /// <param name="clipboardAccessToken">IClipboardLock</param>
         /// <param name="text">string to place on the clipboard</param>
-        /// <param name="format"></param>
-        public static void SetAsUnicodeString(this IClipboardAccessToken clipboardAccessToken, string text, string format = "CF_UNICODETEXT")
+        /// <param name="format">StandardClipboardFormats with the clipboard format to use</param>
+        public static void SetAsUnicodeString(this IClipboardAccessToken clipboardAccessToken, string text, StandardClipboardFormats format)
+        {
+            clipboardAccessToken.SetAsUnicodeString(text, (uint)format);
+        }
+
+        /// <summary>
+        /// Place string on the clipboard, this assumes you already locked the clipboard.
+        /// </summary>
+        /// <param name="clipboardAccessToken">IClipboardLock</param>
+        /// <param name="text">string to place on the clipboard</param>
+        /// <param name="format">string with the clipboard format to use</param>
+        public static void SetAsUnicodeString(this IClipboardAccessToken clipboardAccessToken, string text, string format)
+        {
+            clipboardAccessToken.SetAsUnicodeString(text, ClipboardFormatExtensions.MapFormatToId(format));
+        }
+
+        /// <summary>
+        /// Place string on the clipboard, this assumes you already locked the clipboard.
+        /// It uses Unicode (CF_UNICODETEXT) by default, as all other formats are automatically generated from this by Windows.
+        /// </summary>
+        /// <param name="clipboardAccessToken">IClipboardLock</param>
+        /// <param name="text">string to place on the clipboard</param>
+        /// <param name="formatId">uint with the clipboard format id</param>
+        public static void SetAsUnicodeString(this IClipboardAccessToken clipboardAccessToken, string text, uint formatId = (uint)StandardClipboardFormats.UnicodeText)
         {
             var unicodeBytes = Encoding.Unicode.GetBytes(text + "\0");
-            clipboardAccessToken.SetAsBytes(unicodeBytes, format);
+            clipboardAccessToken.SetAsBytes(unicodeBytes, formatId);
+        }
+
+        /// <summary>
+        /// Get a string from the clipboard, this assumes you already locked the clipboard.
+        /// </summary>
+        /// <param name="clipboardAccessToken">IClipboardLock</param>
+        /// <param name="format">StandardClipboardFormats with the clipboard format</param>
+        /// <returns>string</returns>
+        public static string GetAsUnicodeString(this IClipboardAccessToken clipboardAccessToken, StandardClipboardFormats format)
+        {
+            return clipboardAccessToken.GetAsUnicodeString((uint)format);
         }
 
         /// <summary>
@@ -46,11 +79,23 @@ namespace Dapplo.Windows.Clipboard
         /// This always takes the CF_UNICODETEXT format, as Windows automatically converts
         /// </summary>
         /// <param name="clipboardAccessToken">IClipboardLock</param>
-        /// <param name="format">string</param>
+        /// <param name="format">string with the clipboard format</param>
         /// <returns>string</returns>
-        public static string GetAsUnicodeString(this IClipboardAccessToken clipboardAccessToken, string format = "CF_UNICODETEXT")
+        public static string GetAsUnicodeString(this IClipboardAccessToken clipboardAccessToken, string format)
         {
-            var bytes = clipboardAccessToken.GetAsBytes(format);
+            return clipboardAccessToken.GetAsUnicodeString(ClipboardFormatExtensions.MapFormatToId(format));
+        }
+
+        /// <summary>
+        /// Get a string from the clipboard, this assumes you already locked the clipboard.
+        /// This by default takes the CF_UNICODETEXT format, as Windows automatically converts
+        /// </summary>
+        /// <param name="clipboardAccessToken">IClipboardLock</param>
+        /// <param name="formatId">uint with the clipboard format</param>
+        /// <returns>string</returns>
+        public static string GetAsUnicodeString(this IClipboardAccessToken clipboardAccessToken, uint formatId = (uint)StandardClipboardFormats.UnicodeText)
+        {
+            var bytes = clipboardAccessToken.GetAsBytes(formatId);
             return Encoding.Unicode.GetString(bytes, 0, bytes.Length).TrimEnd('\0');
         }
     }
