3cc69c242b9aff10bb7d2555abfd8d9eeca438d0
Robin Krom <robin@getgreenshot.org>
2021-03-30 22:55:55 +0200

    Fixed an issue with dependencies in using projects.

--- src/Dapplo.Windows.Tests.Benchmarks/Program.cs
+++ src/Dapplo.Windows.Tests.Benchmarks/Program.cs
@@ -15,15 +15,29 @@ namespace Dapplo.Windows.Tests.Benchmarks
         public static void Main(string[] args)
         {
 
-            //var jobCore50 = Job.Default.WithRuntime(CoreRuntime.Core50).WithPlatform(Platform.X64);
-            //var jobNet472 = Job.Default.WithRuntime(ClrRuntime.Net472).WithPlatform(Platform.X64);
-            //var config = DefaultConfig.Instance.AddJob(jobCore50).AddJob(jobNet472);
-            BenchmarkRunner.Run<ScreenboundsBenchmark>();
+            var jobCore50 = Job.Default
+                .WithMaxIterationCount(20)
+                .WithRuntime(CoreRuntime.Core50)
+                .WithPlatform(Platform.X64);
+            var jobCore31 = Job.Default
+                .WithMaxIterationCount(20)
+                .WithRuntime(CoreRuntime.Core31)
+                .WithPlatform(Platform.X64);
+            var jobNet472 = Job.Default
+                .WithMaxIterationCount(20)
+                .WithRuntime(ClrRuntime.Net472)
+                .WithPlatform(Platform.X64);
+            var config = DefaultConfig.Instance
+                    .AddJob(jobCore50)
+                    .AddJob(jobCore31)
+                    .AddJob(jobNet472)
+                ;
 
-            //BenchmarkRunner.Run<ClipboardBenchmarks>();
-            //BenchmarkRunner.Run<EnumerateWindowsBenchmark>();
-            //BenchmarkRunner.Run<InteropWindowBenchmark>();
-            Console.ReadKey();
+            BenchmarkRunner.Run<ScreenboundsBenchmark>(config);
+            BenchmarkRunner.Run<ClipboardBenchmarks>(config);
+            BenchmarkRunner.Run<EnumerateWindowsBenchmark>(config);
+            BenchmarkRunner.Run<InteropWindowBenchmark>(config);
+            Console.ReadLine();
         }
     }
 }
